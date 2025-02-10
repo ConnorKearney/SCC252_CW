6691124b1b837328f6fe3ece4213110eb34f1832
Connor Kearney <c.kearney@lancaster.ac.uk>
2025-02-10 14:11:35 +0000

    Basic layout from coursework spec

---
+++ SCC252_CW/main.py
@@ -0,0 +1,17 @@
+# START OF YOUR SUBMISSION 
+# Include any required modules here 
+
+def aes_encrypt(key, plaintext): 
+# Implement AES encryp on instan ated in CBC mode.  
+# You should use PKCS7 for padding.   
+# You should use a 16-byte random IV.  
+    return #IV and ciphertext 
+
+
+def generate_hmac(key, data): 
+# Implement HMAC.  
+# You must select an appropriate hash func on for your implementa on. 
+    return #MAC 
+
+if __name__ == "__main__":
+    pass
